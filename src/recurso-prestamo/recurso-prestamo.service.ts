37e29b65928128e7a3fa8de5b290d9a0f7ce7aa1
Julio Pena <granjajajaja@Julios-MacBook-Pro.local>
2024-12-01 02:22:39 -0500

    Se agregaron todos los test de logica de la relacion recurso-prestamo

--- src/recurso-prestamo/recurso-prestamo.service.ts
+++ src/recurso-prestamo/recurso-prestamo.service.ts
@@ -34,8 +34,10 @@ export class RecursoPrestamoService
         let prestado = 0;
         // Se calcula cuanto de un recurso esta actualmente prestado sumando todos los valores de los prestamos asociados al recurso.
         for(let i = 0; i < recurso.prestamos.length; i++)
-            {
-                prestado += recurso.prestamos[i].monto;
+            {   
+                // Se verifica que el prestamo no haya sido pagado.
+                if(!recurso.prestamos[i].pagado)
+                  prestado += recurso.prestamos[i].monto;
             }
         // Se verifica que la suma de los prestamos asociados al recurso no superen su valor.
         if(recurso.valor - prestado < prestamo.monto)
@@ -51,13 +53,13 @@ export class RecursoPrestamoService
         const prestamo: PrestamoEntity = await this.prestamoRepository.findOne({where: {id: prestamoId}});
         // Se verifica que el prestamo exista.
         if (!prestamo)
-          throw new BusinessLogicException("No se encontro el prestamo con el Id dado ", BusinessError.NOT_FOUND)
+          throw new BusinessLogicException("No se encontro el prestamo con el Id dado", BusinessError.NOT_FOUND)
        
-        const recurso: RecursoEntity = await this.recursoRepository.findOne({where: {id: recursoId}});
+        const recurso: RecursoEntity = await this.recursoRepository.findOne({where: {id: recursoId}, relations: ["prestamos"]});
         // Se verifica que el recurso exista.
         if (!recurso)
           throw new BusinessLogicException("No se encontro el recurso con el Id dado", BusinessError.NOT_FOUND)
-   
+        
         const recursoPrestamo: PrestamoEntity = recurso.prestamos.find(e => e.id === prestamo.id);
         // Se verifica que el prestamo este asociado al recurso.
         if (!recursoPrestamo)
@@ -86,13 +88,18 @@ export class RecursoPrestamoService
           throw new BusinessLogicException("No se encontro el recurso con el Id dado", BusinessError.NOT_FOUND)
     
         // Se verifica que los prestamos existan.
+        let prestado = 0;
         for (let i = 0; i < prestamos.length; i++) 
         {
           const prestamo: PrestamoEntity = await this.prestamoRepository.findOne({where: {id: prestamos[i].id}});
           if (!prestamo)
-            throw new BusinessLogicException("No se encontro un prestamo con uno de los Id dados ", BusinessError.NOT_FOUND)
+            throw new BusinessLogicException("No se encontro alguno de los prestamos con los Id dados", BusinessError.NOT_FOUND)
+          if(!prestamo.pagado)
+            prestado += prestamo.monto;
         }
-
+        // Se verifica que los prestamos no superen el valor del recurso.
+        if(recurso.valor < prestado)
+          throw new BusinessLogicException("Los montos prestados superan el valor del recurso disponible", BusinessError.PRECONDITION_FAILED);
         recurso.prestamos = prestamos;
         return await this.recursoRepository.save(recurso);
       }
@@ -100,10 +107,10 @@ export class RecursoPrestamoService
     // Desasociar un prestamo de un recurso.
     async deletePrestamoRecurso(recursoId: string, prestamoId: string)
     {
-        const prestamo: PrestamoEntity = await this.prestamoRepository.findOne({where: {id: prestamoId}, relations: ["pagos"]});
+        const prestamo: PrestamoEntity = await this.prestamoRepository.findOne({where: {id: prestamoId}});
         // Se verifica que el prestamo exista.
         if (!prestamo)
-          throw new BusinessLogicException("No se encontro el prestamo con el Id dado ", BusinessError.NOT_FOUND)
+          throw new BusinessLogicException("No se encontro el prestamo con el Id dado", BusinessError.NOT_FOUND)
     
         const recurso: RecursoEntity = await this.recursoRepository.findOne({where: {id: recursoId}, relations: ["prestamos"]});
         // Se verifica que el recurso exista.
@@ -116,18 +123,27 @@ export class RecursoPrestamoService
         if (!recursoPrestamo)
             throw new BusinessLogicException("El prestamo con el Id dado no esta asociado al recurso", BusinessError.PRECONDITION_FAILED)
  
-        //Calculamos la cantidad de capital pagado del prestamo.
-        let pagado = 0;
-        for(let i = 0; i < prestamo.historialpagos.length; i++)
-        {
-            pagado += prestamo.historialpagos[i].capital;
-        }
-        // Se verifica que el prestamo haya sido pagado
-        if(pagado < prestamo.monto)
-            throw new BusinessLogicException("El prestamo no puede ser desasociado debido a que no se ha pagado la totalidad del capital prestado", BusinessError.PRECONDITION_FAILED);
-
+        //Se mira si el prestamo esta pagado
+        if(!recursoPrestamo.pagado)
+            throw new BusinessLogicException("El prestamo con el Id dado no esta pagado", BusinessError.PRECONDITION_FAILED)
+        
         recurso.prestamos = recurso.prestamos.filter(e => e.id !== prestamoId);
         await this.recursoRepository.save(recurso);
     }  
+
+    // Borrar un Recurso, se hace aca porque no puede borrarse un recurso si tiene prestamos asociados.
+    async deleteRecurso(recursoId: string)
+    {
+        const recurso: RecursoEntity = await this.recursoRepository.findOne({where: {id: recursoId}, relations: ["prestamos"]});
+        // Se verifica que el recurso exista.
+        if (!recurso)
+          throw new BusinessLogicException("No se encontro el recurso con el Id dado", BusinessError.NOT_FOUND)
+        
+        // Se verifica que el recurso no tenga prestamos asociados.
+        if(recurso.prestamos.length > 0)
+          throw new BusinessLogicException("No se puede borrar el recurso porque tiene prestamos asociados", BusinessError.PRECONDITION_FAILED)
+        
+        await this.recursoRepository.delete(recursoId);
+    }
 
 }
