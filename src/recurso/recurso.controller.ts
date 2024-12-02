857a8390e5e3b6b4df4c6a230b8e9714bac5955b
Julio Pena <granjajajaja@Julios-MacBook-Pro.local>
2024-12-02 11:44:17 -0500

    Seguridad implementada para recursos y recurso-prestamo

--- src/recurso/recurso.controller.ts
+++ src/recurso/recurso.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { RecursoService } from './recurso.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { RecursoDto } from './recurso.dto';
 import { RecursoEntity } from './recurso.entity';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { Roles } from 'src/user/roles.decorator';
+import { Role } from 'src/user/role.enum';
 
 @Controller('recursos')
 @UseInterceptors(BusinessErrorsInterceptor)
@@ -13,18 +16,24 @@ export class RecursoController {
     ) {}
 
     @Get()
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.PRESTAMISTA,Role.DEUDOR, Role.ADMIN)
     async findAll() 
     {
         return await this.recursoService.findAll();
     }
 
     @Get(':recursoId')
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.PRESTAMISTA,Role.DEUDOR, Role.ADMIN)
     async findOne(@Param('recursoId') recursoId: string)
     {
         return await this.recursoService.findOne(recursoId);
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.PRESTAMISTA,Role.DEUDOR, Role.ADMIN)
     async create(@Body()recursoDto: RecursoDto)
     {
         const recurso : RecursoEntity = plainToInstance(RecursoEntity, recursoDto)
@@ -32,6 +41,8 @@ export class RecursoController {
     }
 
     @Put(':recursoId')
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.PRESTAMISTA,Role.DEUDOR, Role.ADMIN)
     async update(@Param('recursoId') recursoId: string, @Body() recursoDto: RecursoDto)
     {
         const recurso : RecursoEntity = plainToInstance(RecursoEntity, recursoDto)
@@ -40,6 +51,8 @@ export class RecursoController {
 
     @Delete('recurso/:recursoId')
     @HttpCode(204)
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.PRESTAMISTA,Role.DEUDOR, Role.ADMIN)
     async delete(@Param('recursoId') recursoId: string)
     {
         return await this.recursoService.delete(recursoId);
